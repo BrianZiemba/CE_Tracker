a5cad198cbc848db1cc41e73e43dd963fde25a43
BrianZiemba <124318722+BrianZiemba@users.noreply.github.com>
2023-03-12 18:09:19 -0500

    Add files via upload

--- App.xaml.cs
+++ App.xaml.cs
@@ -19,25 +19,39 @@ namespace CE_Tracker
     /// </summary>
     public partial class App : Application
     {
-        private static readonly ILog log = Helper.GetLoggerRollingFileAppender("TestAppender", "D:/log.txt");
+        // relative path to App_Data from debug folder
+        private static readonly string appDataLoc = "../../../App_Data";
+        //log4net
+        private static readonly ILog log = Helper.GetLoggerRollingFileAppender("TestAppender", "log.txt");
+        
 
         protected override void OnStartup(StartupEventArgs e)
         {
-           
-            
-            log.Info("LocalPath= " + Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory + "../../../App_Data"));
-            Debug.WriteLine(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory + "../../../App_Data"));
-            var appPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "cetrack\\App_Data");
-            var localPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data"); 
-            Debug.WriteLine($"localPath = {localPath}");
-            Debug.WriteLine("appPath = " + appPath);
-            log.Info("appPath = " + appPath);
-            
+
+            log.Info("LocalPath = " + Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory + appDataLoc));
+            Debug.WriteLine("LocalPath = " + Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory + appDataLoc));
+            Debug.WriteLine("Base: " + AppDomain.CurrentDomain.BaseDirectory);
+
+            // not using
+            //var appPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "cetrack\\App_Data");
+#if DEBUG
+            var localPath = Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory + appDataLoc);
+#else // release version will have different path to App_Data (will not have to go up multiple dirs from debug folder)
+            var localPath = Path.Join(AppDomain.CurrentDomain.BaseDirectory, "App_Data"); 
+#endif
+
+                      
+            // set DataDirectory location
             AppDomain.CurrentDomain.SetData("DataDirectory", localPath); // appPath
-        
+            
+            log.Info("Data Directory set to: " + AppDomain.CurrentDomain.GetData("DataDirectory"));
             Debug.WriteLine("Data Directory set to: " + AppDomain.CurrentDomain.GetData("DataDirectory"));
 
-            //Debug.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+            // Debug.WriteLine($"localPath = {localPath}");
+            // Debug.WriteLine("appPath = " + appPath);
+            // log.Info("appPath = " + appPath);
+
+
         }
 
 
